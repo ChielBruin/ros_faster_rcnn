c48ac4301f6643243634f81d93d4ba226cd451cf
Chiel Bruin <chielbruin@tele2.nl>
2016-11-03 19:37:30 +0100

    Improved simpleDetector
    Added basic ros structure.
    The node can receive images and run the detection on it, but does not yet publish the results

--- src/simpleDetect.py
+++ src/simpleDetect.py
@@ -24,78 +24,75 @@ import scipy.io as sio
 import caffe, os, sys, cv2
 import argparse
 
-CLASSES = ('__background__',
-           'aeroplane', 'bicycle', 'bird', 'boat',
-           'bottle', 'bus', 'car', 'cat', 'chair',
-           'cow', 'diningtable', 'dog', 'horse',
-           'motorbike', 'person', 'pottedplant',
-           'sheep', 'sofa', 'train', 'tvmonitor')
+import rospy
+from ros_faster_rcnn.msg import Detection
+from sensor_msgs.msg import Image
+
+CLASSES = []
 
 NETS = {'vgg16': ('VGG16',
                   'VGG16_faster_rcnn_final.caffemodel'),
         'zf': ('ZF',
-                  'ZF_faster_rcnn_final.caffemodel')}
+                  'ZF_faster_rcnn_final.caffemodel'),
+        'classes': 'classes.txt'}
 
 
-def vis_detections(im, class_name, dets, thresh=0.5):
-    """Draw detected bounding boxes."""
-    inds = np.where(dets[:, -1] >= thresh)[0]
-    if len(inds) == 0:
-        return
+#def vis_detections(im, class_name, dets, thresh=0.5):
+    #"""Draw detected bounding boxes."""
+    #inds = np.where(dets[:, -1] >= thresh)[0]
+    #if len(inds) == 0:
+        #return
 
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
-    for i in inds:
-        bbox = dets[i, :4]
-        score = dets[i, -1]
+    #im = im[:, :, (2, 1, 0)]
+    #fig, ax = plt.subplots(figsize=(12, 12))
+    #ax.imshow(im, aspect='equal')
+    #for i in inds:
+        #bbox = dets[i, :4]
+        #score = dets[i, -1]
 
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-            )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
+        #ax.add_patch(
+            #plt.Rectangle((bbox[0], bbox[1]),
+                          #bbox[2] - bbox[0],
+                          #bbox[3] - bbox[1], fill=False,
+                          #edgecolor='red', linewidth=3.5)
+            #)
+        #ax.text(bbox[0], bbox[1] - 2,
+                #'{:s} {:.3f}'.format(class_name, score),
+                #bbox=dict(facecolor='blue', alpha=0.5),
+                #fontsize=14, color='white')
 
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                  fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
+    #ax.set_title(('{} detections with '
+                  #'p({} | box) >= {:.1f}').format(class_name, class_name,
+                                                  #thresh),
+                  #fontsize=14)
+    #plt.axis('off')
+    #plt.tight_layout()
+    #plt.draw()
 
-def demo(net, image_name):
-    """Detect object classes in an image using pre-computed object proposals."""
+def imageCallback(im):
+	global NET
+	"""Detect object classes in an image using pre-computed object proposals."""
 
-    # Load the demo image
-    im_file = os.path.join(cfg.DATA_DIR, 'demo', image_name)
-    im = cv2.imread(im_file)
+	# Detect all object classes and regress object bounds
+	timer = Timer()
+	timer.tic()
+	scores, boxes = im_detect(NET, im)
+	timer.toc()
+	print ('Detection took {:.3f}s for '
+		   '{:d} object proposals').format(timer.total_time, boxes.shape[0])
 
-    # Detect all object classes and regress object bounds
-    timer = Timer()
-    timer.tic()
-    scores, boxes = im_detect(net, im)
-    timer.toc()
-    print ('Detection took {:.3f}s for '
-           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
-
-    # Visualize detections for each class
-    CONF_THRESH = 0.8
-    NMS_THRESH = 0.3
-    for cls_ind, cls in enumerate(CLASSES[1:]):
-        cls_ind += 1 # because we skipped background
-        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
-        cls_scores = scores[:, cls_ind]
-        dets = np.hstack((cls_boxes,
-                          cls_scores[:, np.newaxis])).astype(np.float32)
-        keep = nms(dets, NMS_THRESH)
-        dets = dets[keep, :]
-        vis_detections(im, cls, dets, thresh=CONF_THRESH)
+	# Visualize detections for each class
+	CONF_THRESH = 0.8
+	NMS_THRESH = 0.3
+	for cls_ind, cls in enumerate(CLASSES[1:]):
+		cls_ind += 1 # because we skipped background
+		cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+		cls_scores = scores[:, cls_ind]
+		dets = np.hstack((cls_boxes,
+						  cls_scores[:, np.newaxis])).astype(np.float32)
+		keep = nms(dets, NMS_THRESH)
+		dets = dets[keep, :]
+		# //TODO Publish here
 
 def parse_args():
     """Parse input arguments."""
@@ -111,41 +108,42 @@ def parse_args():
     args = parser.parse_args()
 
     return args
+    
+def parseClasses(classFile):
+	global CLASSSES
+	with open(classFile) as f:
+		content = f.readlines()
+	CLASSES = ['__background__'] + map(lambda x: x[:-1], content)
 
 if __name__ == '__main__':
-    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+	cfg.TEST.HAS_RPN = True  # Use RPN for proposals
 
-    args = parse_args()
+	args = parse_args()
 
-    prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
-                            'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
-    caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
-                              NETS[args.demo_net][1])
+	pub_detections = rospy.Publisher('rcnn/detections', Detection, queue_size = 10)
+	rospy.init_node('simpleDetect')
+	rospy.Subscriber("rcnn/image_raw", Image, imageCallback)
 
-    if not os.path.isfile(caffemodel):
-        raise IOError(('{:s} not found.\nDid you run ./data/script/'
-                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
+	prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0], 'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
+	caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models', NETS[args.demo_net][1])
+	classes = parseClasses(os.path.join(cfg.DATA_DIR, 'faster_rcnn_models', NETS['classes']))
 
-    if args.cpu_mode:
-        caffe.set_mode_cpu()
-    else:
-        caffe.set_mode_gpu()
-        caffe.set_device(args.gpu_id)
-        cfg.GPU_ID = args.gpu_id
-    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+	if not os.path.isfile(caffemodel):
+		raise IOError(('{:s} not found.\nDid you run ./data/script/'
+					   'fetch_faster_rcnn_models.sh?').format(caffemodel))
 
-    print '\n\nLoaded network {:s}'.format(caffemodel)
+	if args.cpu_mode:
+		caffe.set_mode_cpu()
+	else:
+		caffe.set_mode_gpu()
+		caffe.set_device(args.gpu_id)
+		cfg.GPU_ID = args.gpu_id
+	NET = caffe.Net(prototxt, caffemodel, caffe.TEST)
 
-    # Warmup on a dummy image
-    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
-    for i in xrange(2):
-        _, _= im_detect(net, im)
-
-    im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-                '001763.jpg', '004545.jpg']
-    for im_name in im_names:
-        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-        print 'Demo for data/demo/{}'.format(im_name)
-        demo(net, im_name)
-
-    plt.show()
+	print '\n\nLoaded network {:s}'.format(caffemodel)
+	print 'Warmup started'
+	im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
+	for i in xrange(2):
+		_, _= im_detect(NET, im)
+	print 'Warmup done. Starting node'
+	rospy.spin()
